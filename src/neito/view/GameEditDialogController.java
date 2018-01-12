c966df54166bc77b3d146a97ae13dbf835d5db22
Neito <willem.houm@gmail.com>
2018-01-12 09:24:42 +0100

    Add check lenght of the name in the Edit dial

--- src/neito/view/GameEditDialogController.java
+++ src/neito/view/GameEditDialogController.java
@@ -1,6 +1,8 @@
 package neito.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import neito.model.Game;
@@ -44,11 +46,40 @@ public class GameEditDialogController {
 	@FXML
 	private void handleOk()
 	{
-		game.setName(nameField.getText());
-		game.setScore(scoreField.getText());
-		game.setEditor(editorField.getText());
+		if(isInputValid())
+		{	
+			game.setName(nameField.getText());
+			game.setScore(scoreField.getText());
+			game.setEditor(editorField.getText());
+			
+			okClicked = true;
+			diagStage.close();
+		}
+	}
+	
+	private boolean isInputValid()
+	{
+		String errorMessage = "";
+		String nF = nameField.getText(), sF = scoreField.getText(), eF = editorField.getText();
+		if(nF == null || nF.length() == 0 || nF.length() > 25) 
+		{
+			errorMessage += "Name empty or too long"; 
+		}
+		if(errorMessage.length() == 0)
+		{
+			return true;
+		}
+		else
+		{
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.initOwner(diagStage);
+			alert.setTitle("Invalid fields");
+			alert.setHeaderText("Please correct invalid fields");
+			alert.setContentText(errorMessage);
+			alert.showAndWait();
+			return false; 
+		}
 		
-		okClicked = true;
-		diagStage.close();
+		
 	}
 }
